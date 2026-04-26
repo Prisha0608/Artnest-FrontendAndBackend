3c66904065db6168e9185da938a8bff2fc40105a
Manureet Kaur <msaluja6754@gmail.com>
2026-04-26 11:56:24 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+// New changes done by Manureet 26 april
+app.use(cors());
+
+// ✅ ADD THIS
+app.use((req, res, next) => {
+  console.log("Request received:");
+  console.log("Method:", req.method);
+  console.log("URL:", req.url);
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// mm
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,6 +40,14 @@ app.use("/cart", cartRoutes);
 app.use("/products", productRoutes);
 app.use("/contacts", contactRoutes);
 app.use("/subscribe", subscriptionRoutes);
+
+
+// ✅ New changes done by Manureet 26 april
+app.use((err, req, res, next) => {
+  console.error("Error:", err.message);
+  res.status(500).json({ message: "Something went wrong" });
+});
+//mm
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
