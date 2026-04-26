e5adaf33e028b6e5fecc9934570ffc626b5c715a
Manureet Kaur <msaluja6754@gmail.com>
2026-04-26 18:14:07 +0530

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// GET USERS
 exports.getUser = async (req, res) => {
   try {
     if (req.query.username) {
@@ -18,7 +17,6 @@ exports.getUser = async (req, res) => {
   }
 };
 
-// REGISTER
 exports.registerUser = async (req, res) => {
   try {
     const { username, contact, password, role, fullname, bio, social } = req.body;
@@ -62,21 +60,18 @@ exports.loginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // compare password
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // generate JWT token
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
 
-    // send response
     res.json({
       token,
       user: {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
-
-// public routes
 router.get("/userDetails", userController.getUser);
 router.post("/signup", userController.registerUser);
 router.post("/login", userController.loginUser);
