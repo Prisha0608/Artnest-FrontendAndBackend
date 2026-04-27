164e735c9eaf1bd8f2d99c161d130276655ee580
Parampreet Kaur <param282006@gmail.com>
2026-04-28 00:25:55 +0530

    ejs

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "./views")
+
+app.get("/home-ejs", (req, res) => {
+  const hour = new Date().getHours();
+
+  let greeting = "Hello";
+  if (hour < 12) greeting = "Good Morning";
+  else if (hour < 18) greeting = "Good Afternoon";
+  else greeting = "Good Evening";
+
+  res.render("home", { greeting });
+});
+
 require("dotenv").config({ path: "./.env" });
 
 const connectDB = require("./database/dbconnection");
